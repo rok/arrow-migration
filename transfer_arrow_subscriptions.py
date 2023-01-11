5945541f634721a0046145ebf3c036e9c991943b
Rok Mihevc <rok@mihevc.org>
2023-01-11 20:16:37 +0100

    subscription script changes

--- transfer_arrow_subscriptions.py
+++ transfer_arrow_subscriptions.py
@@ -3,6 +3,7 @@ import requests
 import csv
 import sys
 
+
 SCRIPT_DESCRIPTION = """
 This script uses GitHub API to subscribe you to migrated Apache Arrow issues.
 You will be prompted for your GitHub username and a GitHub token with notification permissions.
@@ -10,13 +11,9 @@ You can create one here: https://github.com/settings/tokens/new
 """
 DATA_URL = "https://raw.githubusercontent.com/rok/arrow-migration/main/issue_subscriptions.csv"
 API_URL = "https://api.github.com/graphql"
-SUBSCRIBE_QUERY = """
-mutation subscribe_to_issues {{
-  updateSubscription(input: {{subscribableId: "{}", state: SUBSCRIBED}}) {{
-    clientMutationId
-  }}
-}}
-"""
+SUBSCRIBE_QUERY = \
+    'mutation {{updateSubscription(input: {{subscribableId: "{}", state: SUBSCRIBED}}) {{clientMutationId}}}}'
+
 
 def get_subscriptions(git_username):
     subscriptions = []
@@ -30,14 +27,16 @@ def get_subscriptions(git_username):
                 subscriptions.append(row)
     return subscriptions
 
-def subscribe(subscriptions):
+
+def subscribe(subscriptions, headers):
     with requests.Session() as s:
-        for subscription in subscriptions:
-            print(subscription[2])
+        for i, subscription in enumerate(subscriptions):
+            print(i, subscription[2])
             query = SUBSCRIBE_QUERY.format(subscription[3])
-            request = requests.post(API_URL, json={'query': query}, headers=HEADERS)
-            if request.status_code != 200:
-                raise Exception(query, "returned", request.status_code, "\nSubscription process not complete.")
+            response = s.post(API_URL, json={'query': query}, headers=headers)
+
+            if response.status_code != 200:
+                raise Exception(query, "returned", response.status_code, "\nSubscription process not complete.")
 
                 
 if __name__ == "__main__":
@@ -51,10 +50,9 @@ if __name__ == "__main__":
     print("Found", len(subscriptions), "issues.")
 
     GITHUB_API_TOKEN = input("Please input your GitHub API token. It should have notification permissions: ")
-    HEADERS = {"Authorization": "token " + GITHUB_API_TOKEN}
 
     print("Subscribing:")
     print("=========================================================")
-    subscribe(subscriptions)
+    subscribe(subscriptions, {"Authorization": "token " + GITHUB_API_TOKEN})
     print("=========================================================")
     print("Successfully subscribed to", len(subscriptions), "GitHub issues.")
